36d5a466597f68d2712fbfb0949ec4effba98818
VISHNU VARDHAN <131098500+Vishnuvardhan799@users.noreply.github.com>
2025-04-06 14:29:22 +0530

    Add files via upload

---
+++ VI-Sem/Sem6 Senior Study Material Prajwal/CSPC62 - Compilers/Aadhi Lab1/1/sample.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// this is      a single line comment
+
+int main()
+{
+    /*This is a multipline
+    COMMENT*/
+
+    
+    printf("Hello World\n");
+    return 0;
+}
---
+++ VI-Sem/Sem6 Senior Study Material Prajwal/CSPC62 - Compilers/Aadhi Lab1/2/sample.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 5;
+    if(a > 0)
+    {
+        while(a--)
+            printf("Good Day\n");
+    }
+    else
+    {
+        for(int i=0;i<a;i++)
+            printf("May Day\n");
+    }
+    printf("hellowerwedfsefefsdfasvhjadhbaljgahfjvdafbladjfgankjganvjanlriugaenirfijadvnkadjfgnaerkjakvnafjanfjafkasdasd");
+    return 0;
+}
